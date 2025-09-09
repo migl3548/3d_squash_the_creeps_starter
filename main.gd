ab1f0d0da9f590e36e9780bdaa46b5945d3df323
Ari <migl3548@colorado.edu>
2025-09-08 22:13:48 -0600

    added retrying

--- main.gd
+++ main.gd
@@ -25,3 +25,12 @@ func _on_mob_timer_timeout():
 
 func _on_player_hit() -> void:
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+	
+func _ready():
+	$UserInterface/Retry.hide()
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
